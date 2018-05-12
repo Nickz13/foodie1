9948848e5beb7b8d140c3d2642fb8c3a5c5f8766
Nickz13 <31153647+Nickz13@users.noreply.github.com>
2018-05-12 12:42:56 +0530

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import {HttpClientModule} from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ModalPage } from '../pages/modal/modal';
+import { ModalPageModule } from '../pages/modal/modal.module';
 
 @NgModule({
   declarations: [
@@ -14,12 +17,16 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpModule,
+    ModalPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ModalPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, ModalOptions } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+   public items: any;
+  constructor(public modal: ModalController, public navCtrl: NavController, public http: HttpClient) {
+    this.getData();
   }
+ 
+  getData() {
+    let url = 'data/register-data.json';
+    let data: Observable<any> = this.http.get(url);
+    data.subscribe(result => {
+      this.items = result;
 
+    });
+  }
+ 
+
+  openModal() {
+      const myModal = this.modal.create("ModalPage");
+      
+      myModal.onDidDismiss(data => {
+        this.items.push(data);
+      });
+      myModal.present();
+  }
+  
+ 
 }
